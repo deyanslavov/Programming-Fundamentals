79e3916145567514bdd9c00886315e2eb8ef8538
deyanslavov <pikacho2013@gmail.com>
2017-09-26 19:41:36 +0300

    Added Data Types and Variables - Exercises

---
+++ Data Types and Variables - Exercises/03. Practice Chars And Strings/03. Practice Chars And Strings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string s1 = "Software University";
+        char b = 'B';
+        char y = 'y';
+        char e = 'e';
+        string s2 = "I love programming";
+
+        Console.WriteLine(s1);
+        Console.WriteLine(b);
+        Console.WriteLine(y);
+        Console.WriteLine(e);
+        Console.WriteLine(s2);
+    }
+}
---
+++ Data Types and Variables - Exercises/04. Variable in Hex Format/04. Variable in Hex Format.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string myHex = Console.ReadLine();
+        int myNewInt = Convert.ToInt32(myHex, 16);
+
+        Console.WriteLine(myNewInt);
+    }
+}
