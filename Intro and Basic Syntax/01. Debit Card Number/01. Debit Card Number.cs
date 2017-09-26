3be5dad29724419a09f19f4cff514f3f1b6e2eef
deyanslavov <pikacho2013@gmail.com>
2017-09-26 18:56:32 +0300

    added first exercise

---
+++ Intro and Basic Syntax/01. Debit Card Number/01. Debit Card Number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_and_Basic_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:d4} {1:d4} {2:d4} {3:d4}", num1, num2, num3, num4);
+        }
+    }
+}
---
+++ Intro and Basic Syntax/04. Beverage Labels/04. Beverage Labels.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.Beverage_Labels
+{
+    using System;
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            var volume = double.Parse(Console.ReadLine());
+            var energy = double.Parse(Console.ReadLine());
+            var sugar = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}ml {1}:\n{2}kcal, {3}g sugars", volume, name, volume*(energy/100), volume*(sugar/100));
+        }
+    }
+}
